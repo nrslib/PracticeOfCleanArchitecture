cd5c6cca5686e7f51d34f8c37165f3e7e9218b06
srn <masanobu.naruse@gmail.com>
2018-09-09 20:10:41 +0900

    add unit tests

---
+++ Src/Domain.Tests/Users/UserCreateCollector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UseCase.Users.Create;
+
+namespace Domain.Tests.Users
+{
+    public class UserCreateCollector : IUserCreatePresenter
+    {
+        public List<int> Percentages { get; } = new List<int>();
+        public UserCreateResponse Response { get; private set; }
+
+        public void Progress(int percentage) {
+            Percentages.Add(percentage);
+        }
+
+        public void Complete(UserCreateResponse response) {
+            Response = response;
+        }
+    }
+}
---
+++ Src/Domain.Tests/Users/UserCreateInteractorTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using Domain.Application.Users;
+using InMemoryInfrastructure.Users;
+using UseCase.Users.Create;
+
+namespace Domain.Tests.Users
+{
+    [TestClass]
+    public class UserCreateInteractorTest
+    {
+        [TestMethod]
+        public void TestCreateUser() {
+            var repository = new InMemoryUserRepository();
+            var presenter = new UserCreateCollector();
+            var interactor = new UserCreateInteractor(repository, presenter);
+            var request = new UserCreateRequest("TestUser");
+
+            interactor.Handle(request);
+            var expectedPercentages = new List<int> {
+                10,
+                30,
+                50,
+                80
+            };
+            Assert.IsTrue(expectedPercentages.SequenceEqual(presenter.Percentages));
+            Assert.IsNotNull(presenter.Response);
+            Assert.IsNotNull(presenter.Response.UserId);
+            var inserted = repository.FindByUserName("TestUser");
+            Assert.IsNotNull(inserted);
+        }
+    }
+}
